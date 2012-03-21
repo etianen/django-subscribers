62e1157adcb4ac99d5109221b2a57bf7a218d4f0
Dave Hall <dave@etianen.com>
2012-03-21 17:00:35 +0000

    Fixing Django 1.4 compatibility

--- src/subscribers/admin.py
+++ src/subscribers/admin.py
@@ -495,6 +495,6 @@ class EmailAdmin(VersionAdminBase):
         
     class Media:
         js = (
-            settings.ADMIN_MEDIA_PREFIX + "js/calendar.js",
-            settings.ADMIN_MEDIA_PREFIX + "js/admin/DateTimeShortcuts.js",
+            settings.STATIC_URL + "js/calendar.js",
+            settings.STATIC_URL + "js/admin/DateTimeShortcuts.js",
         )
